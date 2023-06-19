2c7ab41a22165c22bf0cefca0409996e4bb24d06
Sheena Bish Tang <101611557+SheaTang@users.noreply.github.com>
2023-06-19 01:53:47 -0500

    Cleaned up photo file

--- home.js
+++ home.js
@@ -4,7 +4,11 @@ function Home() {
       bgcolor="black"
       txtcolor="black"
       header={
-        <img src="svbLarge.png" className="img-fluid" alt="Responsive image" />
+        <img
+          src="image/SVB_land.jpg"
+          className="img-fluid"
+          alt="Responsive image"
+        />
       }
       title="Welcome to Bad Banking!"
       text="You can move around using the navigation bar."
